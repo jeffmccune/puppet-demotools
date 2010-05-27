78f0da0d39a320c7f038396006ea82ed86b58ba6
Jeff McCune <jeff@puppetlabs.com>
2010-05-27 22:55:34 +0200

    Manage the timezone

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,11 +1,26 @@
 notice("puppet-demotools, jeff@puppetlabs.com, Twitter: 0xEFF")
 
+# $my_timezone = "America/New_York"
+$my_timezone = "Europe/Brussels"
+
+File { owner => "root", group => "root", mode => "0644" }
+
 # Set hostname
 puppet::hostname {
   'puppet':
     domainname => 'demo.lan',
     before => [ Class["puppetdemotools"],
                 Class["puppetdemotools::splunk"], ]
+}
+
+file {
+  "/etc/localtime":
+    ensure => "/usr/share/zoneinfo/Europe/Brussels"
+}
+
+file {
+  "/etc/sysconfig/clock":
+    content  => "ZONE=\"${my_timezone}\"\nUTC=true\nARC=false\n"
 }
 
 include puppetdemotools
