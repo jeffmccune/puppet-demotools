e878569e8f4c3d95aaa1c40502a43601e62f25bb
Jeff McCune <jeff@puppetlabs.com>
2010-07-02 15:09:31 -0700

    Added passenger module.
    Added passenger and ruby puppet modules from the Forge.

--- manifests/setup-puppet-testing.pp
+++ manifests/setup-puppet-testing.pp
@@ -1,12 +1,16 @@
 # Settings
-
 $hostname_configured = $ipaddress ? {
     "192.168.100.148" => "hyel",
     "192.168.100.149" => "gunab",
     default => "unknown",
 }
-
-notice("Setting Hostname to: ${hostname_configured}")
+$testing_role = $hostname_configured ? {
+    "hyel"  => "master",
+    default => "agent",
+}
+notice("Configured hostname: [${hostname_configured}]")
+notice("Configured role: [${hostname_configured}]")
+#################################################
 
 # Set the hostname of the system
 puppet::hostname {
@@ -14,11 +18,14 @@ puppet::hostname {
         domainname => "${domain}";
 }
 
+# Stuff to make Jeff happy...
 include jeffprefs
+# The actual testing infrastructure module
 include puppettesting
-
 # We need gcc for Passenger
 include gcc
 # We need apache for Passenger
 include apache
+# Passenger
+include passenger
 
