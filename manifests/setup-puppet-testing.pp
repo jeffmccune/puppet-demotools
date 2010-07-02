a5b434cefb45d04ae862c4badcccd118604ed085
Jeff McCune <jeff@puppetlabs.com>
2010-07-02 13:39:42 -0700

    Set hostname based on IP address.
    Also added gcc, which will be required for passenger setup.

--- manifests/setup-puppet-testing.pp
+++ manifests/setup-puppet-testing.pp
@@ -1,3 +1,21 @@
+# Settings
+
+$hostname_configured = $ipaddress ? {
+    "192.168.100.148" => "hyel",
+    "192.168.100.149" => "gunab",
+    default => "unknown",
+}
+
+notice("Setting Hostname to: ${hostname_configured}")
+
+puppet::hostname {
+    "${hostname_configured}":
+        domainname => "${domain}";
+}
+
 include jeffprefs
 include puppettesting
+
+# We need gcc
+include gcc
 
