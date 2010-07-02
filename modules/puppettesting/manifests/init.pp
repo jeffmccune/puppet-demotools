be5a22369457b33de831051072fb3fe7fd296bb5
Jeff McCune <jeff@puppetlabs.com>
2010-07-01 17:06:11 -0700

    Initial integration with puppet_spec
    Provide a local configuration file for initial integration
    With the puppet_spec framework for external acceptance testing.

--- modules/puppettesting/manifests/init.pp
+++ modules/puppettesting/manifests/init.pp
@@ -1,9 +1,17 @@
-#
+# JJM Puppet Testing Module
+# This module sets up a host to run performance
+# testing.
 
 class puppettesting {
-    $basedir = "/opt/puppetlabs"
+    # JJM Place stuff in /opt/puppetlabs
+    $optdir = $fact_optdir ? { false => "/opt/puppetlabs", default => "${fact_optdir}", }
+    # Comes from facter environment variable in puppet-runtime script
+    $demodir_real = $demodir ? { false => "/demo", default => "${demodir}", }
+    $demotools_real = $demotools ? { false => "puppet-demotools", default => "${demotools}", }
+    # STATIC Variables
     $puppeturl = "http://github.com/reductivelabs/puppet.git"
     $facterurl = "http://github.com/reductivelabs/facter.git"
+    # Resource Defaults
     File {
         ensure => directory,
         mode   => "0644",
@@ -13,19 +21,26 @@ class puppettesting {
     Vcsrepo {
         ensure => present,
         provider => "git",
-        require => [ File["${basedir}"] ],
+        require => [ File["${optdir}"] ],
     }
-####
+#### Resource Declarations
     file {
-        [ "/opt", "${basedir}" ]:;
+        [ "/opt", "${optdir}" ]:;
     }
     vcsrepo {
-        "${basedir}/puppet":
+        "${optdir}/puppet":
             source => "${puppeturl}";
-        "${basedir}/facter":
+        "${optdir}/facter":
             source => "${facterurl}";
-        "${basedir}/puppet_spec":
+        "${optdir}/puppet_spec":
             source => "http://github.com/jes5199/puppet_spec.git";
+    }
+    file {
+        "${optdir}/puppet_spec":
+            require => Vcsrepo["${optdir}/puppet_spec"];
+        "${optdir}/puppet_spec/local_setup.sh":
+            ensure => file,
+            content => "source /demo/puppet-demotools/resources/environment\n";
     }
 }
 
