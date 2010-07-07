5813c165446c1064a056cac48db83e580941afee
Jeff McCune <jeff@puppetlabs.com>
2010-07-06 18:25:59 -0700

    We need apache SSL

--- modules/proj/puppettesting/manifests/master/passenger.pp
+++ modules/proj/puppettesting/manifests/master/passenger.pp
@@ -14,7 +14,8 @@
 #
 class puppettesting::master::passenger inherits puppettesting::master {
     $module = "puppettesting"
-    include "apache"
+    include apache
+    include apache::ssl
     # Variables
     # The location of the passenger module
     $passenger_module_path = "/usr/lib/ruby/gems/1.8/gems/passenger-2.2.15/ext/apache2/mod_passenger.so"
